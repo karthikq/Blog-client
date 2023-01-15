4e850e8847b170cfa5413193f07b82cd78cf18c7
karthikq <kk968346@gmail.com>
2023-01-15 12:05:33 +0530

    firebase up

--- src/Firebase.js
+++ src/Firebase.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 // Get a reference to the storage service, which is used to create references in your storage bucket
 const firebaseApp = () => {
   const firebaseConfig = {
-    apiKey: "AIzaSyDLik-kjtTRHg4v6jsgcYnhEfKV8KfmoWw",
-    authDomain: "stoked-cosine-331604.firebaseapp.com",
-    projectId: "stoked-cosine-331604",
-    storageBucket: "stoked-cosine-331604.appspot.com",
-    messagingSenderId: "126929670377",
-    appId: "1:126929670377:web:1f546d6a3e675fe90c2078",
+    apiKey: "AIzaSyBmN-ZcaVanZHz7M9lkB43YephmUtIWJMg",
+    authDomain: "reacttest-c067b.firebaseapp.com",
+    projectId: "reacttest-c067b",
+    storageBucket: "reacttest-c067b.appspot.com",
+    messagingSenderId: "463517770405",
+    appId: "1:463517770405:web:384ef6983458f07735e34c",
   };
 
   const app = initializeApp(firebaseConfig);
